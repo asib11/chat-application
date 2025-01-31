adfd230fc17e156e358f74fbff471756a0af96a5
asib ahmed <ahmedasib1@gmail.com>
2025-01-31 23:29:30 +0600

    feat #5 : login functionality with cookie, jwt

--- router/inboxRouter.js
+++ router/inboxRouter.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const {getInbox} = require('../controller/inboxController')
-const decoratedHtml = require('../middleware/common/decorateHtml')
+const decoratedHtml = require('../middleware/common/decorateHtml');
+const { checkLogin } = require("../middleware/common/checkLogin");
 
 const router = express.Router()
 
-router.get('/',decoratedHtml('inbox'), getInbox);
+router.get('/',decoratedHtml('inbox'),checkLogin, getInbox);
 
 module.exports = router;
